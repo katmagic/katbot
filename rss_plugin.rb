c6db1816ea3580896460d2243cc5449920914762
katmagic <the.magical.kat@gmail.com>
2011-05-20 02:30:03 -0400

    Make simple_parse_feed() much simpler and more robust.

--- rss_plugin.rb
+++ rss_plugin.rb
@@ -1,7 +1,6 @@
 #!/usr/bin/env ruby
 require 'rss'
-require 'net/http'
-require 'uri'
+require 'open-uri'
 
 # Parse a feed into a Hash, which will have a singleton attribute .title
 # containing the title of the feed. Its keys will be article titles, and its
@@ -9,21 +8,13 @@ require 'uri'
 # Each String has a singleton attribute .date containing the date it was
 # last updated.
 def simple_parse_feed(feed_url)
-	parsed = URI.parse(feed_url)
-
-	con = Net::HTTP.new(parsed.host, parsed.port)
-	con.use_ssl = (parsed.scheme = "https")
-
-	feed = RSS::Parser.parse(con.get(parsed.path).body)
+	feed = RSS::Parser.parse(open(feed_url).read(), false)
 
 	res = Hash.new
-	res.define_singleton_method(:title){ feed.title && feed.title.content }
+	res.define_singleton_method(:title){ feed.channel.title }
 	feed.items.each do |a|
-		continue if not (a.title and a.title.content and a.link and a.link.href)
-
-		res[a.title.content] = a.link.href
-		date = a.updated.content
-		a.link.href.define_singleton_method(:date){ date }
+		res[a.title] = a.link
+		res[a.title].define_singleton_method(:date){ a.date }
 	end
 	res
 end
