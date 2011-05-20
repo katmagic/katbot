8da1891f756d2757984253205b380da9e7251201
katmagic <the.magical.kat@gmail.com>
2011-05-20 01:26:55 -0400

    Add a dont_attack list to Assault.

--- assault_plugin.rb
+++ assault_plugin.rb
@@ -1,5 +1,7 @@
 #!/usr/bin/env ruby
 # This is a rather silly script that will assault people on command.
+# It takes one option, 'dont_attack', which is an Array of nicknames not to
+# assault.
 
 require 'cinch'
 
@@ -13,6 +15,12 @@ module Cinch::Plugins
 		ANIMALS = open(LOCAL_DIR + 'animals.txt').lines.map(&:strip)
 		ASSAULT_METHODS = open(LOCAL_DIR + 'assault_methods.txt').lines.map(&:strip)
 
+		DEFAULT_CONF = {
+			'dont_attack' => [
+				'katmagic'
+			]
+		}
+
 		include Cinch::Plugin
 
 		match /assault (.*)/, method: :assault
@@ -21,6 +29,12 @@ module Cinch::Plugins
 			if not m.channel.has_user?(victim)
 				m.safe_reply("I couldn't seem to find #{victim}.")
 				return
+			elsif config['dont_attack'].include?(victim)
+				m.safe_reply("I couldn't possibly attack #{victim}!")
+
+				# Retaliate.
+				return if config['dont_attack'].include?(m.user)
+				victim = m.user
 			end
 
 			assault_method = ASSAULT_METHODS[ rand(ASSAULT_METHODS.length) ]
